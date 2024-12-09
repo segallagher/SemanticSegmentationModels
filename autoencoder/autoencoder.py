658e2d019e3d88e5f29048a308fb1057ad734504
Samuel Gallagher <segallagher@alaska.edu>
2024-12-09 00:12:15 -0900

    feat: add ability to load already trained models and customization for training parameters

--- autoencoder/autoencoder.py
+++ autoencoder/autoencoder.py
@@ -1,5 +1,7 @@
 from keras import losses, optimizers
 from keras.metrics import OneHotMeanIoU
+from keras.models import load_model, Functional
+import tensorflow as tf
 from pathlib import Path
 from utils import load_data, create_fcn, DiceCoefficient, LogBestEpoch
 from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
@@ -31,32 +33,58 @@ data_dir = curr_dir / hyperparam["data_path"]
 train_images, train_labels, val_images, val_labels, test_images = load_data(data_dir, hyperparam["num_classes"], hyperparam["color_mapping"])
 
 
-# Create Model
-model = create_fcn(hyperparam["input_shape"], hyperparam["num_classes"],
-                   hyperparam["encoder_layer_sizes"], hyperparam["bottleneck_size"], hyperparam["decoder_layer_sizes"],
-                   hyperparam["conv_per_block"], hyperparam["kernel_size"], hyperparam["activation"], hyperparam["padding"])
+# Allow GPU memory growth to avoid running out of GPU memory
+# code from https://www.tensorflow.org/guide/gpu
+gpus = tf.config.list_physical_devices('GPU')
+if gpus:
+  try:
+    # Currently, memory growth needs to be the same across GPUs
+    for gpu in gpus:
+        tf.config.experimental.set_memory_growth(gpu, True)
+    logical_gpus = tf.config.list_logical_devices('GPU')
+    print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+  except RuntimeError as e:
+    # Memory growth must be set before GPUs have been initialized
+    print(e)
 
+# Create model metrics
 meaniou_metric = OneHotMeanIoU(num_classes=hyperparam["num_classes"])
 dice_coef = DiceCoefficient()
 optimizer = optimizers.Adam(learning_rate=hyperparam["learning_rate"])
+
+# Load model if model_path is provided, useful for continuing training
+# Otherwise create a new model
+model = None
+if hyperparam.get("model_path"):
+    model: Functional = load_model(hyperparam["model_path"], custom_objects={"DiceCoefficient": dice_coef})
+else:
+    model = create_fcn(hyperparam["input_shape"], hyperparam["num_classes"],
+                   hyperparam["encoder_layer_sizes"], hyperparam["bottleneck_size"], hyperparam["decoder_layer_sizes"],
+                   hyperparam["conv_per_block"], hyperparam["kernel_size"], hyperparam["activation"], hyperparam["padding"])
 model.compile(optimizer=optimizer, loss=losses.categorical_crossentropy, metrics=['accuracy', meaniou_metric, dice_coef])
 model.summary()
 
 # Callbacks
-
-earlystopping = EarlyStopping(monitor="one_hot_mean_io_u", patience=3, mode="max")
-# checkpoint = ModelCheckpoint(filepath=hyperparam["output_name"], monitor="one_hot_mean_io_u", save_best_only=True, mode="max")
-lr_scheduler = ReduceLROnPlateau(monitor="one_hot_mean_io_u", factor=0.5, patience=3)
+earlystopping = EarlyStopping(monitor="one_hot_mean_io_u", patience=10, mode="max")
+checkpoint = ModelCheckpoint(filepath=hyperparam["output_name"], monitor="one_hot_mean_io_u", save_best_only=True, mode="max", verbose=1)
+lr_scheduler = ReduceLROnPlateau(monitor="one_hot_mean_io_u", factor=0.9, patience=6)
 log_best_epoch = LogBestEpoch('one_hot_mean_io_u', ['accuracy', "dice_coefficient"], output_name="autoencoder_metrics.json")
 
 
+# Get training parameters from hyperparameters
+initial_epoch = 0
+if hyperparam.get("initial_epoch"):
+    initial_epoch = hyperparam["initial_epoch"]
+create_fcn
+batch_size = 2
+if hyperparam.get("batch_size"):
+    initial_epoch = hyperparam["batch_size"]
+
+max_epochs = 5000
+if hyperparam.get("max_epochs"):
+    initial_epoch = hyperparam["max_epochs"]
+
 # Train Model
 model.fit(x=train_images, y=train_labels, 
-          epochs=5000, batch_size=2, callbacks=[earlystopping, lr_scheduler, log_best_epoch],
-          shuffle=True, validation_data=(val_images, val_labels)
-        )
-
-
-# Save Model
-print("Training complete, saving model")
-model.save(hyperparam["output_name"])
+            epochs=max_epochs, batch_size=batch_size, callbacks=[earlystopping, checkpoint, lr_scheduler, log_best_epoch],
+            shuffle=True, validation_data=(val_images, val_labels), initial_epoch=initial_epoch)
--- unet/unet.py
+++ unet/unet.py
@@ -1,5 +1,7 @@
 from keras import losses, optimizers
 from keras.metrics import OneHotMeanIoU
+from keras.models import load_model, Functional
+import tensorflow as tf
 from pathlib import Path
 from utils import load_data, create_unet, DiceCoefficient, LogBestEpoch
 from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
@@ -34,30 +36,58 @@ else:
     data_dir = curr_dir / "data"
 train_images, train_labels, val_images, val_labels, test_images = load_data(data_dir, hyperparam["num_classes"], hyperparam["color_mapping"])
 
+# Allow GPU memory growth to avoid running out of GPU memory
+# code from https://www.tensorflow.org/guide/gpu
+gpus = tf.config.list_physical_devices('GPU')
+if gpus:
+  try:
+    # Currently, memory growth needs to be the same across GPUs
+    for gpu in gpus:
+        tf.config.experimental.set_memory_growth(gpu, True)
+    logical_gpus = tf.config.list_logical_devices('GPU')
+    print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+  except RuntimeError as e:
+    # Memory growth must be set before GPUs have been initialized
+    print(e)
 
-model = create_unet(hyperparam["input_shape"], hyperparam["num_classes"],
-                   hyperparam["initial_filters"], hyperparam["depth"], hyperparam["conv_per_block"],
-                   hyperparam["kernel_size"], hyperparam["activation"], hyperparam["padding"])
-
+# Create model metrics
 meaniou_metric = OneHotMeanIoU(num_classes=hyperparam["num_classes"])
 dice_coef = DiceCoefficient()
 optimizer = optimizers.Adam(learning_rate=hyperparam["learning_rate"])
+
+# Load model if model_path is provided, useful for continuing training
+# Otherwise create a new model
+model = None
+if hyperparam.get("model_path"):
+    model: Functional = load_model(hyperparam["model_path"], custom_objects={"DiceCoefficient": dice_coef})
+else:
+    model = create_unet(hyperparam["input_shape"], hyperparam["num_classes"],
+                   hyperparam["initial_filters"], hyperparam["max_depth"], hyperparam["conv_per_block"],
+                   hyperparam["kernel_size"], hyperparam["activation"], hyperparam["padding"])
 model.compile(optimizer=optimizer, loss=losses.categorical_crossentropy, metrics=['accuracy', meaniou_metric, dice_coef])
 model.summary()
 
 # Callbacks
-
-earlystopping = EarlyStopping(monitor="one_hot_mean_io_u", patience=3, mode="max")
-# checkpoint = ModelCheckpoint(filepath=hyperparam["output_name"], monitor="one_hot_mean_io_u", save_best_only=True, mode="max")
-lr_scheduler = ReduceLROnPlateau(monitor="one_hot_mean_io_u", factor=0.5, patience=3)
+earlystopping = EarlyStopping(monitor="one_hot_mean_io_u", patience=10, mode="max")
+checkpoint = ModelCheckpoint(filepath=hyperparam["output_name"], monitor="one_hot_mean_io_u", save_best_only=True, mode="max", verbose=1)
+lr_scheduler = ReduceLROnPlateau(monitor="one_hot_mean_io_u", factor=0.9, patience=6)
 log_best_epoch = LogBestEpoch('one_hot_mean_io_u', ['accuracy', "dice_coefficient"], output_name="unet_metrics.json")
 
 
-# Train Model
-model.fit(x=train_images, y=train_labels,
-            epochs=5000, batch_size=4, callbacks=[earlystopping, log_best_epoch],
-            shuffle=True, validation_data=(val_images, val_labels))
+# Get training parameters from hyperparameters
+initial_epoch = 0
+if hyperparam.get("initial_epoch"):
+    initial_epoch = hyperparam["initial_epoch"]
 
-# Save Model
-print("Training complete, saving model")
-model.save(hyperparam["output_name"])
+batch_size = 2
+if hyperparam.get("batch_size"):
+    initial_epoch = hyperparam["batch_size"]
+
+max_epochs = 5000
+if hyperparam.get("max_epochs"):
+    initial_epoch = hyperparam["max_epochs"]
+
+# Train Model
+model.fit(x=train_images, y=train_labels, 
+            epochs=max_epochs, batch_size=batch_size, callbacks=[earlystopping, checkpoint, lr_scheduler, log_best_epoch],
+            shuffle=True, validation_data=(val_images, val_labels), initial_epoch=initial_epoch)
